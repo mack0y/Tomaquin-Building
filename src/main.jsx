3f2ddf1c78c6aa905482d258990b45a8077a182c
mack0y <mackoysemailadd@gmail.com>
2026-06-24 13:53:26 +0800

    Fix HashRouter basename for GitHub Pages subpath routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <HashRouter basename="/Tomaquin-Building">
       <ToastProvider>
         <App />
       </ToastProvider>
